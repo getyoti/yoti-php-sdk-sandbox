90ea251a7f2f5d13e3394410cbc4fca7e0559c73
David Grayston <davidgrayston@googlemail.com>
2020-04-30 17:56:53 +0100

    SDK-1523: Deprecate optional parameter

--- src/Profile/Request/Attribute/SandboxAttribute.php
+++ src/Profile/Request/Attribute/SandboxAttribute.php
@@ -17,9 +17,6 @@ class SandboxAttribute implements \JsonSerializable
     /** @var string */
     protected $derivation;
 
-    /** @var bool */
-    protected $optional;
-
     /** @var \Yoti\Sandbox\Profile\Request\Attribute\SandboxAnchor[] */
     protected $anchors;
 
@@ -27,20 +24,27 @@ class SandboxAttribute implements \JsonSerializable
      * @param string $name
      * @param string $value
      * @param string $derivation
-     * @param bool $optional
      * @param \Yoti\Sandbox\Profile\Request\Attribute\SandboxAnchor[] $anchors
      */
     public function __construct(
         string $name,
         string $value,
         string $derivation = '',
-        bool $optional = false,
-        array $anchors = []
+        $anchors = []
     ) {
         $this->name = $name;
         $this->value = $value;
         $this->derivation = $derivation;
-        $this->optional = $optional;
+
+        $args = func_get_args();
+        if (isset($args[3]) && is_bool($args[3])) {
+            @trigger_error(
+                'Boolean argument 4 passed to ' . __METHOD__ . ' is deprecated in 1.1.0 ' .
+                'and will be removed in 2.0.0',
+                E_USER_DEPRECATED
+            );
+            $anchors = $args[4] ?? [];
+        }
 
         Validation::isArrayOfType($anchors, [\Yoti\Sandbox\Profile\Request\Attribute\SandboxAnchor::class], 'anchors');
         $this->anchors = $anchors;
@@ -55,7 +59,6 @@ class SandboxAttribute implements \JsonSerializable
             'name' => $this->name,
             'value' => $this->value,
             'derivation' => $this->derivation,
-            'optional' => $this->optional,
             'anchors' => $this->anchors,
         ];
     }
