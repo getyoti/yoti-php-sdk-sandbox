a0751a0b66b9d93f80fb2ed0290daf201f83fd7c
David Grayston <davidgrayston@googlemail.com>
2020-05-28 15:56:52 +0100

    SDK-1544: Use Dotenv\Dotenv for profile example

--- examples/profile/bootstrap.php
+++ examples/profile/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
+use Dotenv\Dotenv;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Symfony\Component\Dotenv\Dotenv;
-
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__ . '/.env');
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
