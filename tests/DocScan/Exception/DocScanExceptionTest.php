76f5279e42f67f7de5dd350e440889342fe84612
David Grayston <davidgrayston@googlemail.com>
2020-06-23 15:16:00 +0100

    SDK-1581: Include formatted response error in exception message

--- tests/DocScan/Exception/DocScanExceptionTest.php
+++ tests/DocScan/Exception/DocScanExceptionTest.php
@@ -6,9 +6,26 @@ use Psr\Http\Message\ResponseInterface;
 use Yoti\Sandbox\DocScan\Exception\SandboxDocScanException;
 use Yoti\Sandbox\Test\TestCase;
 
+use function GuzzleHttp\Psr7\stream_for;
+
 class SandboxDocScanExceptionTest extends TestCase
 {
-    private const SOME_ERROR_MESSAGE = 'Some Error Message';
+    private const SOME_MESSAGE = 'Some message';
+    private const SOME_RESPONSE_CODE = 'SOME_ERROR_CODE';
+    private const SOME_RESPONSE_MESSAGE = 'Some response message';
+    private const SOME_PROPERTY = 'some.property';
+    private const SOME_PROPERTY_MESSAGE = 'Some property message';
+    private const SOME_OTHER_PROPERTY = 'some.other.property';
+    private const SOME_OTHER_PROPERTY_MESSAGE = 'Some other property message';
+
+    /**
+     * @test
+     */
+    public function responseShouldBeOptional()
+    {
+        $docScanException = new SandboxDocScanException(self::SOME_MESSAGE);
+        $this->assertEquals(self::SOME_MESSAGE, $docScanException->getMessage());
+    }
 
     /**
      * @test
@@ -17,8 +34,176 @@ class SandboxDocScanExceptionTest extends TestCase
     {
         $responseMock = $this->createMock(ResponseInterface::class);
 
-        $docScanException = new SandboxDocScanException(self::SOME_ERROR_MESSAGE, $responseMock);
-        $this->assertEquals(self::SOME_ERROR_MESSAGE, $docScanException->getMessage());
+        $docScanException = new SandboxDocScanException(self::SOME_MESSAGE, $responseMock);
+        $this->assertEquals(self::SOME_MESSAGE, $docScanException->getMessage());
+        $this->assertSame($responseMock, $docScanException->getResponse());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldExcludeNonJsonResponsesFromMessage()
+    {
+        $responseMock = $this->createMock(ResponseInterface::class);
+        $responseMock->method('hasHeader')->willReturn(true);
+        $responseMock->method('getHeader')->willReturn(['text/html']);
+
+        $docScanException = new SandboxDocScanException(self::SOME_MESSAGE, $responseMock);
+
+        $this->assertEquals(self::SOME_MESSAGE, $docScanException->getMessage());
         $this->assertEquals($responseMock, $docScanException->getResponse());
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider jsonResponseMessageDataProvider
+     */
+    public function shouldIncludeFormattedResponseInMessage($message, $jsonData, $expectedMessage)
+    {
+        $responseMock = $this->createMock(ResponseInterface::class);
+        $responseMock->method('hasHeader')->willReturn(true);
+        $responseMock->method('getHeader')->willReturn(['application/json']);
+        $responseMock->method('getBody')->willReturn(stream_for(json_encode($jsonData)));
+
+        $docScanException = new SandboxDocScanException($message, $responseMock);
+
+        $this->assertEquals($expectedMessage, $docScanException->getMessage());
+        $this->assertEquals($responseMock, $docScanException->getResponse());
+    }
+
+    /**
+     * Provides JSON response data and their expected exception message.
+     *
+     * @return array
+     */
+    public function jsonResponseMessageDataProvider(): array
+    {
+        return [
+            /**
+             * Message and code.
+             */
+            [
+                self::SOME_MESSAGE,
+                [
+                    'message' => self::SOME_RESPONSE_MESSAGE,
+                    'code' => self::SOME_RESPONSE_CODE
+                ],
+                sprintf(
+                    '%s - %s - %s',
+                    self::SOME_MESSAGE,
+                    self::SOME_RESPONSE_CODE,
+                    self::SOME_RESPONSE_MESSAGE
+                ),
+            ],
+            /**
+             * Message, code and errors with some unknown items
+             */
+            [
+                self::SOME_MESSAGE,
+                [
+                    'message' => self::SOME_RESPONSE_MESSAGE,
+                    'code' => self::SOME_RESPONSE_CODE,
+                    'errors' => [
+                        [
+                            'some_unknown_item' => 'some unknown item',
+                            'message' => self::SOME_PROPERTY_MESSAGE,
+                        ],
+                        [
+                            'property' => self::SOME_PROPERTY,
+                            'some_unknown_item' => 'some unknown item',
+                        ],
+                        [
+                            'property' => self::SOME_OTHER_PROPERTY,
+                            'message' => self::SOME_OTHER_PROPERTY_MESSAGE,
+                        ],
+                    ],
+                ],
+                sprintf(
+                    '%s - %s - %s: %s "%s"',
+                    self::SOME_MESSAGE,
+                    self::SOME_RESPONSE_CODE,
+                    self::SOME_RESPONSE_MESSAGE,
+                    self::SOME_OTHER_PROPERTY,
+                    self::SOME_OTHER_PROPERTY_MESSAGE
+                ),
+            ],
+            /**
+             * Message, code and errors with unknown items
+             */
+            [
+                self::SOME_MESSAGE,
+                [
+                    'message' => self::SOME_RESPONSE_MESSAGE,
+                    'code' => self::SOME_RESPONSE_CODE,
+                    'errors' => [
+                        [
+                            'some_unknown_item' => 'some unknown item',
+                            'message' => self::SOME_PROPERTY_MESSAGE,
+                        ],
+                        [
+                            'property' => self::SOME_PROPERTY,
+                            'some_unknown_item' => 'some unknown item',
+                        ],
+                    ],
+                ],
+                sprintf(
+                    '%s - %s - %s',
+                    self::SOME_MESSAGE,
+                    self::SOME_RESPONSE_CODE,
+                    self::SOME_RESPONSE_MESSAGE
+                ),
+            ],
+            /**
+             * Message, code and errors.
+             */
+            [
+                self::SOME_MESSAGE,
+                [
+                    'message' => self::SOME_RESPONSE_MESSAGE,
+                    'code' => self::SOME_RESPONSE_CODE,
+                    'errors' => [
+                        [
+                            'property' => self::SOME_PROPERTY,
+                            'message' => self::SOME_PROPERTY_MESSAGE,
+                        ],
+                        [
+                            'property' => self::SOME_OTHER_PROPERTY,
+                            'message' => self::SOME_OTHER_PROPERTY_MESSAGE,
+                        ],
+                    ],
+                ],
+                sprintf(
+                    '%s - %s - %s: %s "%s", %s "%s"',
+                    self::SOME_MESSAGE,
+                    self::SOME_RESPONSE_CODE,
+                    self::SOME_RESPONSE_MESSAGE,
+                    self::SOME_PROPERTY,
+                    self::SOME_PROPERTY_MESSAGE,
+                    self::SOME_OTHER_PROPERTY,
+                    self::SOME_OTHER_PROPERTY_MESSAGE
+                ),
+            ],
+            /**
+             * Message without code.
+             */
+            [
+                self::SOME_MESSAGE,
+                [
+                    'message' => self::SOME_RESPONSE_MESSAGE,
+                ],
+                self::SOME_MESSAGE,
+            ],
+            /**
+             * Code without message.
+             */
+            [
+                self::SOME_MESSAGE,
+                [
+                    'code' => self::SOME_RESPONSE_CODE,
+                ],
+                self::SOME_MESSAGE,
+            ],
+        ];
     }
 }
