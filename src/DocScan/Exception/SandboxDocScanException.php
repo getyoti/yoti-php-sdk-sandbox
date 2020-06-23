f60942f5dda2936d75381f8dac49634b4278355e
David Grayston <davidgrayston@googlemail.com>
2020-06-23 15:16:00 +0100

    SDK-1581: Include formatted response error in exception message

--- src/DocScan/Exception/SandboxDocScanException.php
+++ src/DocScan/Exception/SandboxDocScanException.php
@@ -5,24 +5,24 @@ declare(strict_types=1);
 namespace Yoti\Sandbox\DocScan\Exception;
 
 use Psr\Http\Message\ResponseInterface;
+use Yoti\Util\Json;
 
 class SandboxDocScanException extends \Exception
 {
-
     /**
      * @var ResponseInterface|null
      */
     private $response;
 
     /**
-     * DocScanException constructor.
      * @param string $message
      * @param ResponseInterface|null $response
      * @param \Throwable|null $previous
      */
-    public function __construct($message = "", ResponseInterface $response = null, \Throwable $previous = null)
+    public function __construct($message = "", ?ResponseInterface $response = null, \Throwable $previous = null)
     {
-        parent::__construct($message, 0, $previous);
+        parent::__construct($this->formatMessage($message, $response), 0, $previous);
+
         $this->response = $response;
     }
 
@@ -35,5 +35,73 @@ class SandboxDocScanException extends \Exception
     public function getResponse(): ?ResponseInterface
     {
         return $this->response;
+    }
+
+    /**
+     * @param string $message
+     * @param ResponseInterface|null $response
+     *
+     * @return string
+     */
+    private function formatMessage(string $message, ?ResponseInterface $response): string
+    {
+        if (
+            $response === null ||
+            !$response->hasHeader('Content-Type') ||
+            $response->getHeader('Content-Type')[0] !== 'application/json'
+        ) {
+            return $message;
+        }
+
+        $jsonData = Json::decode((string) $response->getBody(), false);
+        $formattedResponse = $this->formatResponse($jsonData);
+        if ($formattedResponse !== null) {
+            return sprintf('%s - %s', $message, $formattedResponse);
+        }
+
+        return $message;
+    }
+
+    /**
+     * @param \stdClass $jsonData
+     *
+     * @return string|null
+     */
+    private function formatResponse(\stdClass $jsonData): ?string
+    {
+        if (!isset($jsonData->message) || !isset($jsonData->code)) {
+            return null;
+        }
+
+        $responseMessage = sprintf('%s - %s', $jsonData->code, $jsonData->message);
+
+        $propertyErrors = $this->formatPropertyErrors($jsonData);
+        if (count($propertyErrors) > 0) {
+            return sprintf('%s: %s', $responseMessage, implode(', ', $propertyErrors));
+        }
+
+        return $responseMessage;
+    }
+
+    /**
+     * @param \stdClass $jsonData
+     *
+     * @return string[]
+     */
+    private function formatPropertyErrors(\stdClass $jsonData): array
+    {
+        if (!isset($jsonData->errors) || !is_array($jsonData->errors)) {
+            return [];
+        }
+
+        return array_filter(array_map(
+            function ($error): ?string {
+                if (isset($error->property) && isset($error->message)) {
+                    return sprintf('%s "%s"', $error->property, $error->message);
+                }
+                return null;
+            },
+            $jsonData->errors
+        ));
     }
 }
